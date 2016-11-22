16731099b32ff81ea325f1c1c1d8c0a8fab476ec
John Dupper <john.dupper@me.com>
2016-11-22 09:51:30 -0500

    trying to manage viewport sizing

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 let controller = {
 
     lastClickTime: 0,
-    timeRemaining: 15,
+    timeRemaining: 35,
     timeInterval: null,
     gamePlay: null,
     gameInProgress: false,
 
     onPlayButtonPressed: function() {
-        if (controller.gameInProgress) {
-            // game in progress, do not start another
-        } else {
+
+        if (!controller.gameInProgress) {
+            
             controller.gameInProgress = true;
-            // start a game
             controller.gamePlay = setInterval(function() {
                 controller.animateGame(game.generateRandomMole());
-            }, 1000);
-            
+            }, 1000); // sets game speed
+
             $('#userScore').text('0');
             $('#playBtn').text('Play Game');
             $('#userTime').text(controller.timeRemaining);
-            // start timer
+
             controller.startTimer();
         }
     },
@@ -34,8 +33,8 @@ let controller = {
         controller.timeRemaining -= 1;
         
         if (controller.timeRemaining < 1) {    
-            // gameInProgress = false
             controller.gameInProgress = false;
+            
             // stop game & timer
             clearInterval(controller.gamePlay);
             clearInterval(controller.timeInterval);
@@ -52,16 +51,16 @@ let controller = {
     },
 
     onMoleClick: function() {
+        // compares times of clicks: if too fast, no second point
         if (event.target.classList.contains('move')) {
             let secondClickTime = event.timeStamp;
             let timeDiff = secondClickTime - controller.lastClickTime;
             
-            if (timeDiff < 400) {
-                console.log('blocked point');
-            } else {
+            if (timeDiff > 400) {
                 game.addPoint();
                 $('#userScore').text(game.currentPoints);
             }
+            
             secondClickTime = 0;
             controller.lastClickTime = event.timeStamp;
         }
@@ -69,31 +68,17 @@ let controller = {
     
     reset: function() {
         game.resetGame();
+        
         // reset controller
         controller.lastClickTime = 0;
-        controller.timeRemaining = 15;
+        controller.timeRemaining = 35;
         controller.timeInterval = null;
         controller.gamePlay = null;
         controller.gameInProgress = false;
         
         $('.mole').removeClass('move');
         // change UI for end of game
-        $('#userTime').text('Game over!');
+        $('#userTime').text('0');
         $('#playBtn').text('Play Again?');
     }
 };
-
-/*
-    right now point scoring is based on .hole div having animate as a class.
-    animation keyframe is tied to the mole itself
-    <hole> --> <mole-box> --> <mole>
-
-
-
-
-
-    :: don't allow button to create many games
-    :: refactor onMoleClick()
-    :: make reset function (game too)
-    :: show high score on game over
-*/
